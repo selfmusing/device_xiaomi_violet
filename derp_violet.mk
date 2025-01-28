b72785ec340766c70bb955c84d670947ae37f53e
selfmusing <mirandamehek@gmail.com>
2025-01-28 14:15:47 +0000

    violet: Bring back Quick Tap

--- derp_violet.mk
+++ derp_violet.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 DERP_BUILDTYPE := Official
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_NOT_USES_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_CALL_RECORDING := true
