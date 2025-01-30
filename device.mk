863463667d454df00379cee54c76430bbe7716fd
Giovanni Ricca <giovanniricca@proton.me>
2025-01-30 19:07:51 +0100

    violet: Simplify FM2 section
    * `qcom.fmradio` and `libqcomfm_jni` are now built by default
    
    Change-Id: Ia7d9d3253fae17c161bfae82a0562dcb73b14964

--- device.mk
+++ device.mk
@@ -237,9 +237,7 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
+    FM2
 
 # Firmware
 $(call inherit-product, vendor/xiaomi-firmware/violet/firmware.mk)
