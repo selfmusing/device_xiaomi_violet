45a8681fe4aba10e093846a863da24f4a514be86
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-02-10 18:18:11 +0530

    Fixup! violet: Switch to AIDL USB HAL
    This fixes commit 49224c6.

--- device.mk
+++ device.mk
@@ -500,7 +500,6 @@ AB_OTA_UPDATER := false
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb-service.qti \
     android.hardware.usb.gadget-service.qti
 
 # Vibrator
