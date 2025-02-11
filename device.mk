356cbea6a3243fa1401609593b65a72a953c5aab
selfmusing <mirandamehek@gmail.com>
2025-02-11 12:42:58 +0530

    Revert "violet: Add prebuilt protobuf 3.9.1 to vendor partition."
    This reverts commit f9f0b0ef96664a9ce1a04694e25614655ac4af6f.

--- device.mk
+++ device.mk
@@ -382,11 +382,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
-
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
@@ -432,6 +427,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
     libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     librmnetctl \
     libxml2 \
     libsqlite.vendor
