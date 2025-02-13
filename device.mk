c5591d16739d383a1739e572f4a93dc30b490417
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 14:48:54 +0800

    violet: Build QTI Thermal AIDL HAL
    Change-Id: I21406af9d90fe6be1402f30e1b19958e4f46eec9

--- device.mk
+++ device.mk
@@ -477,6 +477,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext \
     xiaomi-telephony-stub
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
