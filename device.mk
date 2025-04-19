3ecb8e70dbca997a0586e99a3ef8301dce80ceae
Joker-V2 <nipinna0@gmail.com>
2025-04-19 04:53:34 +0000

    violet: Use libhidlbase-v32 from lineage/compat

--- device.mk
+++ device.mk
@@ -275,6 +275,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidlbase-v32 \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
@@ -487,10 +488,6 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
-
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
