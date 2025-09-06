c4158181a860f639f1f63e355b984336a3a88479
Joker-V2 <nipinna0@gmail.com>
2025-09-07 01:56:07 +0530

    violet: livedisplay: Strip Out Picture Adjustment & AntiFlicker
    Co-authored-by: Selfmuser <109387456+selfmusing@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -221,10 +221,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Soong
 SOONG_CONFIG_NAMESPACES += xiaomiSm6150Vars
-SOONG_CONFIG_xiaomiSm6150Vars += \
-    livedisplay_support_anti_flicker \
-    livedisplay_support_sunlight_enhancement
-SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_anti_flicker ?= true
+SOONG_CONFIG_xiaomiSm6150Vars += livedisplay_support_sunlight_enhancement
 SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_sunlight_enhancement ?= true
 
 # WiFi
