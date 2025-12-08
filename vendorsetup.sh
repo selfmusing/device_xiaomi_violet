2019346f70bd7980e9d94e3cb717d7018c8df252
Selfmuser <109387456+selfmusing@users.noreply.github.com>
2025-12-09 01:13:53 +0530

    violet: shift to Xcalibur kernel for QPR1
    ~ All credits goes to @Joker-V2 :)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone --depth=1 https://github.com/selfmusing/platform_vendor_xiaomi_violet.git -b 16 vendor/xiaomi/violet
-git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone https://github.com/selfmusing/android_kernel_xiaomi_violet.git -b sixteen kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
 git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
