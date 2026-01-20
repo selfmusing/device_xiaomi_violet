b2ac6ca22dc0fc364cab9aa5f51b01c1d1b13b0d
Selfmuser <109387456+selfmusing@users.noreply.github.com>
2026-01-21 04:24:18 +0530

    violet: update remove_pkg targets for QPR2

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,11 +4,69 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AmbientStreaming AppDirectedSMSService Drive ConnMO OdadPrebuilt CalculatorGoogle DCMO CalendarGoogle DeskClock Dialer Etar Glimpse Jelly Messaging Contacts Gallery2 Gemini 
-LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera HealthIntelligenceStubPrebuilt KidsSupervisionStub MaestroPrebuilt Maps MyVerizonServices EmergencyInfoGms
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap Photos PixelSupportPrebuilt PlayAutoInstallConfig SwitchAccessPrebuilt Gmail2 FamilyLinkParentalControls PersonalSafety
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM talkback Tycho USCCDM VZWAPNLib Flash
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger VoiceAccessPrebuilt OBDM_Permissions obdm_stub DevicePolicyPrebuilt
+
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    AmbientStreaming \
+    AppDirectedSMSService \
+    CalculatorGooglePrebuilt_85006267 \
+    CalendarGooglePrebuilt \
+    ConnMO \
+    Contacts \
+    DCMO \
+    DeskClock \
+    DevicePolicyPrebuilt-v10334460 \
+    Dialer \
+    DMService \
+    Drive \
+    Etar \
+    FamilyLinkParentalControls \
+    Flash \
+    Gallery2 \
+    GCS \
+    GeminiShell_227 \
+    Gmail2 \
+    Glimpse \
+    GoogleCamera \
+    GoogleContacts \
+    HealthIntelligenceStubPrebuilt \
+    Jelly \
+    KidsSupervisionStub \
+    MaestroPrebuilt \
+    Maps \
+    Messaging \
+    MlkitBarcodeUIPrebuilt \
+    MyVerizonServices \
+    obdm_stub \
+    OBDM_Permissions \
+    OdadPrebuilt \
+    Photos \
+    PixelSupportPrebuilt \
+    PlayAutoInstallConfig \
+    PrebuiltPixelCoreServices \
+    RelationshipsPrebuilt-301 \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt_v8.4.773573318 \
+    SearchSelectorPrebuilt \
+    SettingsIntelligence \
+    Showcase \
+    Snap \
+    SoundAmplifierPrebuilt_v4.8.758106410 \
+    SprintDM \
+    SprintHM \
+    SwitchAccessPrebuilt_1.16.0.726766860 \
+    talkback \
+    Tycho \
+    UdfpsAnimations \
+    UdfpsIcons \
+    USCCDM \
+    VisionBarcodePrebuilt \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
