f877c6dfd625dc15bd371d0bb52ecf5c309a8cf5
Selfmuser <109387456+selfmusing@users.noreply.github.com>
2026-01-25 03:36:30 +0530

    violet: Update Dolby Repo: https://github.com/adi8900/hardware_dolby/commits/lunaris

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,5 @@ git clone https://github.com/selfmusing/android_kernel_xiaomi_violet.git -b sixt
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 # git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
 git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
-rm -rf hardware/xiaomi && git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && rm -rf hardware/xiaomi/dolby && rm -rf hardware/xiaomi/DSPVolumeSynchronizer
-rm -rf hardware/dolby && git clone https://github.com/selfmusing/android_hardware_dolby -b 16.0 hardware/dolby
+rm -rf hardware/xiaomi && git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka-no-dolby hardware/xiaomi && rm -rf hardware/xiaomi/DSPVolumeSynchronizer
+rm -rf hardware/dolby && git clone https://github.com/adi8900/hardware_dolby -b lunaris hardware/dolby
