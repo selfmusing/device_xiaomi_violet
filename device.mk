6f4951144e52bb22f72e53f6dac0bd695a146ed3
Selfmuser <109387456+selfmusing@users.noreply.github.com>
2026-02-10 18:34:51 +0530

    violet: Drop BCR support

--- device.mk
+++ device.mk
@@ -134,9 +134,6 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
-# BCR
-$(call inherit-product, vendor/bcr/bcr.mk)
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,5 @@ git clone --depth=1 https://github.com/selfmusing/platform_vendor_xiaomi_violet.
 rm -rf kernel/xiaomi/violet && git clone --depth=1 https://github.com/selfmusing/android_kernel_xiaomi_violet.git -b sixteen kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 # git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-rm -rf vendor/bcr && git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
 rm -rf hardware/xiaomi && git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka-no-dolby hardware/xiaomi && rm -rf hardware/xiaomi/DSPVolumeSynchronizer
 rm -rf hardware/dolby && git clone https://github.com/adi8900/hardware_dolby -b lunaris hardware/dolby
