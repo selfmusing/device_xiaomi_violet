3698d1a5f8c34ea9bf5bdfb125bec9a0267fd8b6
achu <achu.github@gmail.com>
2026-05-18 22:25:22 +0530

    violet : Fix removal path for DSPVolumeSynchronizer in vendorsetup.sh
    Updated the removal command for DSPVolumeSynchronizer package.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ git clone --depth=1 https://github.com/selfmusing/platform_vendor_xiaomi_violet.
 rm -rf kernel/xiaomi/violet && git clone --depth=1 https://github.com/selfmusing/android_kernel_xiaomi_violet.git -b sixteen kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 # git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-rm -rf hardware/xiaomi && git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka-no-dolby hardware/xiaomi && rm -rf hardware/xiaomi/DSPVolumeSynchronizer
+rm -rf hardware/xiaomi && git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka-no-dolby hardware/xiaomi && rm -rf hardware/xiaomi/packages/DSPVolumeSynchronizer
 rm -rf hardware/dolby && git clone https://github.com/adi8900/hardware_dolby -b lunaris hardware/dolby
